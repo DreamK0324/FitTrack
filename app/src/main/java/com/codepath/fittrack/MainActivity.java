70a4b82581ffd3e660fec93bdef0cd73a2e3ba84
DreamK0324 <dreamk0324@gmail.com>
2021-10-28 18:28:04 -0400

    Optimization of MainActivity

--- app/src/main/java/com/codepath/fittrack/MainActivity.java
+++ app/src/main/java/com/codepath/fittrack/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,39 +15,45 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        BottomNavigationItemView bottomNavigationView = findViewById(R.id.bottom_navigation);
-//
-//
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                Fragment fragment;
-//                switch (item.getItemId()) {
-//                    case R.id.nav_meal:
-//                        fragment = new MealFragment();
-//                        break;
-//                    case R.id.nav_feed:
-//                        fragment = new FeedFragment();
-//                        break;
-//                    case R.id.nav_user:
-//                        fragment = new UserFragment();
-//                        break;
-//                    default:
-//                        fragment = new HomeFragment();
-//                        break;
-//                }
-//                fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
-//                return true;
-//            }
-//        });
-//        // Set default selection
-//        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_meal:
+                        fragment = new MealFragment();
+                        Toast.makeText(MainActivity.this, "Meal", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_feed:
+                        fragment = new FeedFragment();
+                        Toast.makeText(MainActivity.this, "Feed", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_user:
+                        fragment = new UserFragment();
+                        Toast.makeText(MainActivity.this, "User", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
 
     }
